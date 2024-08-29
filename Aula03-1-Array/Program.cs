1d0ea2102adafe3f05b7c79e0d75a02cb7187d0a
Lucas Zanotto <lucasabatizanotto@gmail.com>
2024-08-28 23:56:50 -0300

    add: tarefa da aula 28/08/2024

--- Aula03-1-Array/Program.cs
+++ Aula03-1-Array/Program.cs
@@ -18,30 +18,53 @@ string[] names = ["Eu", "Tu", "Ele", "Nós", "Vós", "Eles"];
 // Exemplo Vetor com os nomes dos meses
 string[] months = new string[12];
 
-for (int i = 1; i <= 12; i++) {
+for (int i = 1; i <= 12; i++)
+{
     DateTime firstDay = new DateTime(DateTime.Now.Year, i, 1);
 
     string name = firstDay.ToString("MMMM");
-    
-    months[i-1] = name;
-} 
 
-foreach(string m in months) {
+    months[i - 1] = name;
+}
+
+foreach (string m in months)
+{
     Console.WriteLine(m);
 }
 
-int[] numberList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-int[] parLista = [];
-int[] imparLista = [];
-int counter = 0;
-foreach(int num in numberList) {
-    if (num % 2 == 0) {
-        parLista[counter] = num;
-    } else {
-        imparLista[counter] = num;
-    }
-    counter++;
+int[] numeros = new int[100];
+
+for (int i = 0; i < 100; i++)
+{
+    numeros[i] = i + 1;
 }
 
-Console.WriteLine(parLista.Length);
-Console.WriteLine(imparLista.Length);
+List<int> listaPares = new List<int>();
+List<int> listaImpares = new List<int>();
+
+foreach (int numero in numeros)
+{
+    if (numero % 2 == 0)
+    {
+        listaPares.Add(numero);
+    }
+    else
+    {
+        listaImpares.Add(numero);
+    }
+}
+
+int[] pares = listaPares.ToArray();
+int[] impares = listaImpares.ToArray();
+
+Console.WriteLine("-------------");
+
+Console.WriteLine("numeros pares:");
+Console.WriteLine(string.Join(", ", pares));
+
+Console.WriteLine("-------------");
+
+Console.WriteLine("numeros impares:");
+Console.WriteLine(string.Join(", ", impares));
+
+Console.WriteLine("-------------");
